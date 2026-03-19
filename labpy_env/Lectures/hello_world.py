22f6d38ffc29bf6c69d6b0e796bf599e95a7a34e
Lorenzo Ilari <lorenzo2002ilari@gmail.com>
2026-03-19 14:50:47 +0100

    Clean project setup

---
+++ labpy_env/Lectures/hello_world.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+import sklearn
+
+print("Hello, World! This is a test of the labpy environment.")
+
+
+print(f"NumPy version: {np.__version__}")
+print(f"Pandas version: {pd.__version__}")
+print(f"Statsmodels version: {sm.__version__}")
+print(f"Scikit-learn version: {sklearn.__version__}")
+
+
