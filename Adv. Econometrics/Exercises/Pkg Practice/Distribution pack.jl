9d232f81790b1bb5583ffccc5d4f316a8fe1e65d
LorenzoIlari <lorenzo2002ilari@gmail.com>
2026-05-06 13:21:56 +0200

    distrib pkg

--- Adv. Econometrics/Exercises/Pkg Practice/Distribution pack.jl
+++ Adv. Econometrics/Exercises/Pkg Practice/Distribution pack.jl
@@ -1,5 +1,5 @@
 using Distributions
-
+see
 ## Generating Distributions
 N = Normal()
 T = TDist(df)
@@ -11,10 +11,14 @@ B = Binomial(n, p)
 
 # for a Normal
 d = Normal()        
-cdf(d, 1.96)        # P(X ≤ 1.96)
-ccdf(d, 1.96)       # P(X > 1.96)
+cdf(d, 1.96)        # P(X ≤ 1.96), Evaluate the cumulative probability at x
+ccdf(d, 1.96)       # P(X > 1.96), the complementary cumulative function evaluated at x, i.e. 1 - cdf(d, x)
 quantile(d, 0.975)  # critic value 97.5%
 pdf(d, 0)           # density in 0
+
 p_val = 2 * ccdf(d, abs(x))
+
+# Estimate parameters
+fit(Normal, x)          # ask which normal distribution best describes it
 
 
